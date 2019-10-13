5d30c77e6d9f0e85eedfcad8de70a73ad6182666
rodollopes <rodollopes@gmail.com>
2019-10-13 20:31:03 -0300

    Adicinado modelo para pesquisa e constantes do Sistema

---
+++ GitAPITeste/Models/ConstantesModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitAPITeste.Models
+{
+    public class ConstantesModel
+    {
+        public const string STRING_INDEFINO = "";
+    }
+}
---
+++ GitAPITeste/Models/PesquisaModel.cs
@@ -0,0 +1,28 @@
+﻿using Octokit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitAPITeste.Models
+{
+    public class PesquisaModel
+    {
+        public string TextoPesquisa { get; }
+        // U - Usuário; E - Empresa
+        public char TipoConta { get; }
+        // L - Login; E - Email; N - Nome
+        public string TipoPesquisa { get; }
+        // A - Ascendente; D - Descendente
+        public char TipoOrdenacao { get; }
+        // S - Seguidores; R - Reposiorios; A - Ambos
+        public char OrdenarPor { get; }
+        public DateTime DataInicio { get; }
+        public DateTime DataFim { get; }
+        // 1 - C#; 2 - Java; 3 - GO
+        public int Linguagem { get; }
+        public string Cidade { get; }
+        public int ResultadosPorPagina { get; }
+        public int Pagina { get; }
+    }
+}
